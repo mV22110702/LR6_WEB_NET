ba309a16278f96e243014695d5bdd8e5e2492e3f
mV22110702 <92586731+mV22110702@users.noreply.github.com>
2024-02-28 22:33:16 +0200

    Added docker-compose

--- LR6_WEB_NET/Data/DatabaseContext/DataContext.cs
+++ LR6_WEB_NET/Data/DatabaseContext/DataContext.cs
@@ -9,6 +9,7 @@ public class DataContext : DbContext
     public DataContext(DbContextOptions<DataContext> options)
         : base(options)
     {
+        Database.EnsureCreated();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
